fac1cc0153ae471518c0b1c33562c612f5fe2d09
h2llo <dy159951@gmail.com>
2023-03-03 19:52:27 +0900

    day 1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mg = require("mongoose");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+mg.connect('mongodb+srv://h2llo:toddlf94@h2llo.muaub68.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
